e2364c74dfd2dab1ef7ba640c642d4d20947b8ca
Lyove <lyove@outlook.com>
2023-12-23 02:19:51 +0800

    init

--- BlogExample/resources/views/layout.blade.php
+++ BlogExample/resources/views/layout.blade.php
@@ -10,17 +10,17 @@
     <!-- Tailwind -->
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
+        /*@import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');*/
 
-        .font-family-karla {
-            font-family: karla;
-        }
+        /*.font-family-karla {*/
+        /*    font-family: karla;*/
+        /*}*/
     </style>
 
     <!-- AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.8.2/alpine.js" defer></script>
     <!-- Font Awesome -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
 </head>
 <body class="bg-white font-family-karla">
 
