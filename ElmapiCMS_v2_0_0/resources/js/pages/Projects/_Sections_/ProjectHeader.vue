cdd6c43b2a284bf04468b235af06d42d1843cd2e
Lyove <lyove@outlook.com>
2024-01-08 00:39:33 +0800

    init

--- ElmapiCMS_v2_0_0/resources/js/pages/Projects/_Sections_/ProjectHeader.vue
+++ ElmapiCMS_v2_0_0/resources/js/pages/Projects/_Sections_/ProjectHeader.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="app__project-common-header fixed w-full flex p-4 border-b bg-white">
-        <!--<span class="flex items-center pr-4">-->
-        <!--    <router-link :to="{name: 'dashborad'}">-->
-        <!--        <i class="fas fa-arrow-left"></i>-->
-        <!--    </router-link>-->
-        <!--</span>-->
-        <div class="w-full">
+        <span class="flex items-center pr-4">
             <router-link :to="{name: 'projects.index', params: { project_id: project.id }}">
-                <div class="text-xl font-bold">
-                    Project: {{ project.name }}
-                </div>
+                <i class="fas fa-arrow-left"></i>
             </router-link>
+        </span>
+        <div class="w-full">
+            <div class="text-xl font-bold">
+                Project: {{ project.name }}
+            </div>
             <div class="text-sm">{{ project.description }}</div>
         </div>
     </div>
