cbd4fac5db8b448f37e165a8f95507b95d0aa754
Lyove <lyove@outlook.com>
2024-01-08 10:46:44 +0000

    init

---
+++ HeadlessCMS/resources/js/pages/Projects/_Sections_/ProjectHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app__project-common-header fixed w-full flex p-4 border-b bg-white">
+        <span class="flex items-center pr-4">
+            <router-link :to="{name: 'projects.index', params: { project_id: project.id }}">
+                <i class="fas fa-arrow-left"></i>
+            </router-link>
+        </span>
+        <div class="w-full">
+            <div class="text-xl font-bold">
+                Project: {{ project.name }}
+            </div>
+            <div class="text-sm">{{ project.description }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import checkRole from '../../../checkrole'
+
+export default {
+    props: ['project'],
+
+    methods: {
+        checkRole
+    }
+}
+</script>
