253377d0a730bbde883148615042ae41d9ebec3e
Lyove <lyove@outlook.com>
2024-01-13 03:22:36 +0800

    init

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -3,7 +3,7 @@
  */
 import Vue from "vue";
 
-import Toast from "vue-toastification";
+import Toast, { createToastInterface } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 import vSelect from "vue-select";
@@ -34,6 +34,59 @@ import store from "./backend/store";
  * Axios middleware
  */
 import "./bootstrap";
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// VueToast
+const VueToast = createToastInterface({
+	draggable: false,
+	timeout: 2000,
+	pauseOnFocusLoss: false,
+	position: "top-center"
+});
+
+// NProgress
+const NProgress = require('nprogress');
+NProgress.configure({ 
+    showSpinner: false
+});
+
+window.axios.interceptors.request.use(config => {
+    NProgress.start()
+    return config
+}, (error) => {
+    NProgress.done();
+    return Promise.reject(error);
+});
+  
+window.axios.interceptors.response.use(response => {
+    NProgress.done()
+    return response
+}, (error) => {
+    if(error.response &&  error.response.status === 401){
+        window.location.reload();
+    } else if(error.response && error.response.status === 403){
+        VueToast.error(error.response.data.message);
+    } else if(error.response &&  error.response.status === 404){
+        VueToast.error("Record not found!");
+    } else if(error.response &&  error.response.status === 419){
+        window.location.reload();
+    } else if(error.response &&  error.response.status === 422){
+        //
+    } else {
+        VueToast.error("An unexpected error occurred!");
+    }
+    
+    NProgress.done();
+
+    return Promise.reject(error);
+});
 
 // Toast
 Vue.use(Toast, {
